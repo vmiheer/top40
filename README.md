3ec480d2aa3e9db79c5b12d0162d5a10e6a5593c
miheer vaidya <v.miheer@gmail.com>
2012-09-11 20:20:04 +0530

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ top40
 =====
 
 a mechanize script to download uk top 40
+Use at your own risk violates SOPA PIPA
+=======================================
+All lows of getting free music :P seriously
+===========================================
+
+Licenced under GPLv3
+
+TODO:
+
+1. http://www.officialcharts.com/singles-chart/ has official chart
+    try to parse
+2. http://www.bbc.co.uk/radio1/chart/singles/print has somewhat simple
+    chart
+3. Check size of file before downloading as mostly they are partial
---
+++ dnld.py
@@ -0,0 +1,56 @@
+# use :%s/^\d\+.*\d\+\s\+//gc 
+# to remove useless thing
+f = open('chart.txt')
+import mechanize
+import cookielib
+import os
+# Browser
+br = mechanize.Browser()
+
+# Cookie Jar
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+# Browser options
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+
+# Follows refresh 0 but not hangs on refresh > 0
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+# Want debugging messages?
+#br.set_debug_http(True)
+#br.set_debug_redirects(True)
+#br.set_debug_responses(True)
+
+# User-Agent (this is cheating, ok?)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+dateProcess = os.popen(r"date +%W-%Y")
+dateProcessOutput = dateProcess.readlines()[0][:-1]
+mkdirPrcess = os.popen(r"mkdir ~/Music/"+dateProcessOutput)
+number = 1
+for line in f:
+  r = br.open('http://google.com')
+  br.select_form(nr=0)
+  br.form['q']=line + " mp3"
+  br.submit()
+  for l in br.links(url_regex='mp3skull.com'):
+    print l.url
+    br.open(l.url)
+    for p in br.links(url_regex='mp3$'):
+      line = line.replace("\t"," ")
+      filename = os.environ['HOME'] + "/Music/"+dateProcessOutput+"/"+str(number)+".mp3"
+      print filename
+      print (br.retrieve(p.url,filename)[0])
+      number = number + 1
+      break
+    break
+  #if l:
+   # print l[0]['url']
+        
+        #r = br.open(l)
+        #print r
